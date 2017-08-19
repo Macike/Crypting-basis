602982606088517939046c482137fb8f2084d135
Viceriel <viceriel@gmail.com>
2017-08-19 12:35:25 +0200

    commiting a message for u

--- training.js
+++ training.js
@@ -5,12 +5,28 @@ window.addEventListener("load",
                            btn.addEventListener("click", message, false);
                         }, false);
 
+/**
+*button click handler
+*
+*@param{Object}
+*/
 function message(e)
 {
   var textbox = document.getElementById("caesar");
-  alert(caesar(textbox.value, 13));
+  var code= caesar(textbox.value, 13);
+  textbox.value=code;
+  var encrypt= julius(code,13);
+  var textboxes = document.getElementById("caesar2");
+  textboxes.value= encrypt;
 }
 
+/**
+*encodes the caesar code
+*
+*@param{String}
+*@param{Number}
+*@return{String}
+*/
 function caesar(sentence, shift)
 {
   sentence = sentence.toLowerCase();
@@ -39,6 +55,44 @@ function caesar(sentence, shift)
     {
         text+= sentence[i];
     }
+  }
+  return text;
+}
+
+/**
+*decodes the caesar code
+*
+*@param{String}
+*@param{Number}
+*@return{String}
+*/
+function julius(sentence, shift)
+{
+  sentence = sentence.toLowerCase();
+  var len=sentence.length;
+  var memmory=0;
+  var text="";
+  var a="a";
+  var z="z";
+  var edge1= a.charCodeAt(0);
+  var edge2= z.charCodeAt(0);
+
+  for(var i=0; i<len; i++)
+  {
+      if(sentence[i]==" ")
+      {
+        text+= sentence[i];
+      }
+      else
+      {
+        memmory= sentence.charCodeAt(i);
+        memmory= memmory - shift;
+        if(memmory<edge1)
+        {
+          memmory+= edge2-edge1+1;
+        }
+        text+= String.fromCharCode(memmory);
+      }
   }
   return text;
 }
